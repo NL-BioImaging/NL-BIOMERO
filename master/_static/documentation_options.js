2736f7dd7675abbb47ca84e0cdd4040630cc3c5a
TorecLuik <TorecLuik@users.noreply.github.com>
2026-07-06 12:14:28 +0000

    deploy: 05e818676cf7d9e878e5fbc87eca507e70513e7b

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.0',
+    VERSION: '1.7.0-beta.12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
